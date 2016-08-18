85fc0ac1d230ad2804bf2ae05d34c70e03f5f7e8
Mark Jordan <mjordan@sfu.ca>
2016-08-18 01:47:45 +0000

    As so often happens, added file that should have been in last commit.

---
+++ theme/islandora-onthisday-rss-channel.tpl.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Islandora-onthisday-rss-channel.tpl.php.
+ *
+ * Template for RSS channel for "on this day" items.
+ */
+?>
+
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+  <title><?php print $site_name; ?></title>
+  <link>http://localhost:8000</link>
+  <description>On this day, <?php print $today; ?></description>
+  <language>en</language>
+  <copyright />
+  <managingEditor />
+  <webMaster />
+<?php if (count($islandora_objects)): ?>
+  <?php foreach($islandora_objects as $item): ?>
+  <item>
+      <title><?php print $item['label'] . ' [' . $item['year'] . ']';?></title>
+      <link><?php print $item['url']; ?></link>
+      <description></description>
+      <author />
+      <guid isPermaLink="false"><?php print $item['pid']; ?></guid>
+      <pubDate />
+      <category />
+      <comments />
+      <source url="<?php print $base_url; ?>"><?php print $site_name; ?></source>
+      <enclosure url="<?php print $item['thumb_link']; ?>" length="<?php print $item['thumb_size']; ?>" type="<?php print $item['mimetype']; ?>" />
+    </item>
+  <?php endforeach; ?>
+<?php endif; ?>
+</channel>
+</rss>
