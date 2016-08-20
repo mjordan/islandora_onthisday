76fc7de8b70f758892edfb38e13fd16c6f7df328
Mark Jordan <mjordan@sfu.ca>
2016-08-20 03:05:33 +0000

    Changed order of admin options; enhanced RSS XML.

--- theme/islandora-onthisday-rss-channel.tpl.php
+++ theme/islandora-onthisday-rss-channel.tpl.php
@@ -7,8 +7,6 @@
  * Template for RSS channel for "on this day" items.
  */
 ?>
-
-<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0">
 <channel>
   <title><?php print $site_name; ?></title>
@@ -17,19 +15,19 @@
   <language>en</language>
   <copyright />
   <managingEditor />
-  <webMaster />
+  <webMaster><?php print $site_mail; ?></webMaster>
 <?php if (count($islandora_objects)): ?>
   <?php foreach($islandora_objects as $item): ?>
   <item>
-      <?php if (strlen($item['year'])): ?>
-      <title><?php print $item['label'] . ' [' . $item['year'] . ']'; ?></title>
-      <?php else: ?>
-      <title><?php print $item['label']; ?></title>
-      <?php endif; ?>
-      <link><?php print $item['url']; ?></link>
-      <description></description>
+  <?php if (strlen($item['year'])): ?>
+    <title><?php print $item['label'] . ' [' . $item['year'] . ']'; ?></title>
+  <?php else: ?>
+    <title><?php print $item['label']; ?></title>
+  <?php endif; ?>
+    <link><?php print $item['url']; ?></link>
+      <description><img src="<?php print $item['thumb_link']; ?>" title="<?php print $item['description']; ?>" alt="<?php print $item['description']; ?>" /></description>
       <author />
-      <guid isPermaLink="false"><?php print $item['pid']; ?></guid>
+      <guid><?php print $item['url']; ?></guid>
       <pubDate />
       <category />
       <comments />
