8a33f55901d961d7ebdd44a7226a50a81cb45a13
Mark Jordan <mjordan@sfu.ca>
2016-09-07 22:16:24 +0000

    Gave the block its own Display collection links admin option.

--- theme/islandora-onthisday-object.tpl.php
+++ theme/islandora-onthisday-object.tpl.php
@@ -3,19 +3,13 @@
 /**
  * @file
  * Islandora-onthisday-object.tpl.php.
+ *
+ * Used only in the block, not the gallery.
  */
 ?>
   <div><a href="<?php print filter_xss($islandora_object['title_link']); ?>"><img src="<?php print $islandora_object['thumb_link']; ?>" /></a></div>
-   <div><a href="<?php print filter_xss($islandora_object['title_link']); ?>"><?php print $islandora_object['label']; ?></a>
-   <?php if (strlen($islandora_object['year'])): ?>
-   (<?php print $islandora_object['year']; ?>)
-   <?php endif; ?>
-   </div>
-   <?php if (count($islandora_object['collections'])): ?>
-     <div id="islandora-onthisday-collection-info">
-     <div id="islandora-onthisday-collections-label"><?php print $islandora_object['collections_label']; ?>:</div>
-       <?php foreach($islandora_object['collections'] as $pid => $label): ?>
-         <div class="islandora-onthisday-collection-link"><a href="/islandora/object/<?php print $pid; ?>"><?php print filter_xss($label); ?></a></div>
-       <?php endforeach; ?>
-     </div>
-   <?php endif; ?>
+  <div><a href="<?php print filter_xss($islandora_object['title_link']); ?>"><?php print $islandora_object['label']; ?></a>
+  <?php if (strlen($islandora_object['year'])): ?>
+    (<?php print $islandora_object['year']; ?>)
+  <?php endif; ?>
+  </div>
